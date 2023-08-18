9926cd36b52505e1679827ffbc72a14b040a0e9c
Prasanna-source31 <139847095+Prasanna-source31@users.noreply.github.com>
2023-08-18 11:29:04 +0530

    Update code_test.tf

--- code_test.tf
+++ code_test.tf
@@ -1,22 +1,9 @@
-provider "aws" {
-  region = "us-west-1"
-}
-
-resource "aws_instance" "example" {
-  ami           = "ami-12345678"
-  instance_type = "t2.micro"
-}
-
-resource "aws_security_group" "example" {
-  name_prefix = "example-"
-}
-
-# This rule has a security issue, causing tfsec to fail
-resource "aws_security_group_rule" "example" {
-  type        = "ingress"
-  from_port   = 0
-  to_port     = 65535
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.example.id
+# Define Terraform backend using a blob storage container on Microsoft Azure for storing the Terraform state
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "my-terraform-rg"
+    storage_account_name = "mytfstorageaccount"
+    container_name       = "my-terraform-state-container"
+    key                  = "terraform.tfstate"
+  }
 }
